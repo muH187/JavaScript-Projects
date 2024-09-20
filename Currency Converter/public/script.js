b3f8af98cd5430ad61a15118153d6b4afed76f82
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-20 13:25:35 +0500

    done

---
+++ Currency Converter/public/script.js
@@ -0,0 +1,46 @@
+const showAmount = document.getElementById("showAmount")
+const input = document.getElementById("input")
+const fromCurrency = document.getElementById("fromCurrency")
+const toCurrency = document.getElementById("toCurrency")
+
+const api = 'api.frankfurter.app';
+
+fetch(`https://${api}/currencies`)
+    .then((data) => data.json())
+    .then((data) => {
+        const entries = Object.entries(data)
+        entries.forEach(([currencyCode, currencyName]) => {
+            const option = `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`;
+            fromCurrency.innerHTML += option
+            toCurrency.innerHTML += option
+        });
+    });
+
+const convert = () => {
+
+    const amount = input.value
+    const fromCurr = fromCurrency.value
+    const toCurr = toCurrency.value
+
+    if (input.value === "") {
+        alert("Input is empty!")
+    } else if (isNaN(input.value)) {
+        alert("Please type valid number!")
+    } else if (Math.sign(input.value) === -1) {
+        alert("Please type positive number only!")
+    } else if (input.value == 0) {
+        alert("Zero is not allowed to convert!")
+    } else if (fromCurr || toCurr === "") {
+        alert("You need to select both 'From Currency' and 'To Currency' options!")
+    } else {
+        fetch(`https://${api}/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`)
+            .then((data) => data.json())
+            .then((data) => {
+                
+                const rate = data.rates[toCurr]
+                showAmount.textContent = `${amount} ${fromCurr} = ${(amount * rate).toFixed(1)} ${toCurr}`
+        
+                
+            })
+    }
+}
