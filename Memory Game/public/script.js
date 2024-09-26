a25c748a96cd69a29e5c0b8c3d32a999d33c85d1
Muhammad Ali <proqualitythings100@gmail.com>
2024-09-26 11:24:00 +0500

    almost done

--- Memory Game/public/script.js
+++ Memory Game/public/script.js
@@ -1,18 +1,69 @@
 const boxContainer = document.querySelector(".box-container")
 
-const emojis = ["😀", "😀", "🤖", "🤖", "🧠", "🧠", "👁", "👁", "💼", "💼", "🥾", "🥾", "🍓", "🍓", "🎯", "🎯"] 
+const emojis = ["😀", "😀", "🤖", "🤖", "🧠", "🧠", "👁", "👁", "💼", "💼", "🥾", "🥾", "🍓", "🍓", "🎯", "🎯"]
 const shuffleEmojis = emojis.sort(() => 0.5 - Math.random())
 
-shuffleEmojis.forEach((emojis) => {
-    
+let firstClick = null
+let secondClick = null
+let lock = false
+let missed = 0
+let matchedPairs = 0
+
+shuffleEmojis.forEach((emoji) => {
+
     const box = document.createElement('div')
-    box.className = "w-24 h-24 bg-blue-400 text-4xl flex justify-center items-center cursor-pointer rounded-md"
-    box.textContent = emojis
+    box.className = "box"
+    box.setAttribute('data-emojis', emoji)
+    box.textContent = ''
 
     box.addEventListener('click', () => {
-        console.log('click...')
+        if (box.textContent !== "" || lock) return
+
+        box.textContent = box.getAttribute('data-emojis')
+
+        if (!firstClick) {
+            firstClick = box
+        } else if (!secondClick) {
+            secondClick = box
+            if (firstClick.getAttribute('data-emojis') === secondClick.getAttribute('data-emojis')) {
+                
+                matchedPairs++
+                firstClick = null
+                secondClick = null
+
+                if(matchedPairs === emojis.length / 2) {
+                    showResult()
+                }
+
+            } else {
+                missed++
+                lock = true
+                setTimeout(() => {
+                    firstClick.textContent = ""
+                    secondClick.textContent = ""
+                    firstClick = null
+                    secondClick = null
+                    lock = false
+                }, 1000);
+            }
+        }
+
+        
+
     })
 
+  
     boxContainer.appendChild(box)
 })
 
+
+const showResult = () => {
+    
+
+    const h1 = document.createElement('h1')
+    h1.textContent = `You missed: ${missed} times!`
+    h1.className = "text-center text-2xl font-semibold text-slate-800"
+    boxContainer.innerHTML = ""
+
+    boxContainer.appendChild(h1)
+}
