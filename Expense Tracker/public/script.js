507ea64ceb012943674a7b6df4a11351fc601a36
Muhammad Ali <proqualitythings100@gmail.com>
2024-10-07 13:34:50 +0500

    working on js

--- Expense Tracker/public/script.js
+++ Expense Tracker/public/script.js
@@ -28,56 +28,117 @@ const submitBtn = document.getElementById('submitBtn')
 
 // Tracking Variables:
 let isExpense = false
-let isIncome = true
+let isIncome = false
 
-submitBtn.addEventListener('click', () => {
-    totalBalance.textContent = `$${100.02}`
-    totalIncome.textContent = `+$${50.93}`
-    totalExpense.textContent = `-$${34.50}`
+window.onload = function(e) {
+    totalBalance.textContent = "$0.00"
+    totalIncome.textContent = "$0.00"
+    totalExpense.textContent = "$0.00"
+}
 
-    const details = document.createElement('div')    
-    details.className = 'px-2 py-1 cursor-pointer hover:bg-slate-300 rounded-md relative'
+submitBtn.addEventListener('click', (displayIncomeAmount, displayExpenseAmount) => {
 
-    const div = document.createElement('div')
-    div.className = ('flex items-center justify-between')
-
-    const div2 = document.createElement('div')
-    
-    const h1 = document.createElement('h1')
-    h1.className = ('font-[600]')
-
-    const p = document.createElement('p')
-
-    if(isIncome) {
-        const displayIncome = document.createElement('h1')
-        displayIncome.className = ('text-xl text-green-700 font-[500]')
+    if (nameInput === "") {
+        alert('You need to type a name!')
+    } else if (amountInput === "") {
+        alert('You need to enter an amount!')
     } else {
-        const displayExpense = document.createElement('h1')
-        displayExpense.className = ('text-xl text-red-700 font-[500]')
+
+        totalBalance.textContent += `$ ${amountInput.value}`
+        totalIncome.textContent += `+$${displayIncomeAmount}`
+        totalExpense.textContent += `-$${displayExpenseAmount}`
+
+        if (isExpense) {
+
+            expenseBtn.addEventListener('click', () => {
+                isExpense = true
+                expenseBtn.className = 'bg-white px-8 font-[500] rounded-md'
+                incomeBtn.classList.remove('bg-white')
+
+                const details = document.createElement('div')
+                details.className = 'px-2 py-1 cursor-pointer hover:bg-slate-300 rounded-md relative'
+
+                const div = document.createElement('div')
+                div.className = ('flex items-center justify-between')
+
+                const div2 = document.createElement('div')
+
+                const h1 = document.createElement('h1')
+                h1.className = ('font-[600]')
+                h1.textContent = nameInput.value
+
+                const p = document.createElement('p')
+
+                // For Image:
+                const div3 = document.createElement('div')
+                div3.className = ('action h-10 w-10 bg-red-600 text-white p-2 rounded-md absolute top-2 right-1')
+
+                const img = document.createElement('img')
+                img.src = 'cross.svg'
+
+                const displayExpenseAmount = document.createElement('h1')
+                displayExpenseAmount.className = ('text-xl text-red-700 font-[500]')
+                displayExpenseAmount.textContent = amountInput.value
+
+                div.appendChild(div2)
+                div.appendChild(displayExpenseName)
+
+                div2.appendChild(h1)
+                div2.appendChild(p)
+
+                div3.appendChild(img)
+
+                details.appendChild(div)
+                details.appendChild(div3)
+            })
+
+            isIncome = true
+
+        } else {
+
+            incomeBtn.addEventListener('click', () => {
+                isIncome = true
+                incomeBtn.className = ('bg-white px-8 font-[500] rounded-md')
+                expenseBtn.classList.remove('bg-white')
+
+
+                const details = document.createElement('div')
+                details.className = 'px-2 py-1 cursor-pointer hover:bg-slate-300 rounded-md relative'
+
+                const div = document.createElement('div')
+                div.className = ('flex items-center justify-between')
+
+                const div2 = document.createElement('div')
+
+                const h1 = document.createElement('h1')
+                h1.className = ('font-[600]')
+                h1.textContent = nameInput.value
+
+                const p = document.createElement('p')
+
+                // For Image:
+                const div3 = document.createElement('div')
+                div3.className = ('action h-10 w-10 bg-red-600 text-white p-2 rounded-md absolute top-2 right-1')
+
+                const img = document.createElement('img')
+                img.src = 'cross.svg'
+
+                const displayIncomeAmount = document.createElement('h1')
+                displayIncomeAmount.className = ('text-xl text-green-700 font-[500]')
+                displayIncomeAmount.textContent = amountInput.value
+
+                div.appendChild(div2)
+                div.appendChild(displayExpenseName)
+
+                div2.appendChild(h1)
+                div2.appendChild(p)
+
+                div3.appendChild(img)
+
+                details.appendChild(div)
+                details.appendChild(div3)
+            })
+        }
+
     }
-    
-    details.appendChild(div)
-    div.appendChild(div2)
-
-    div2.appendChild(h1)
-    div2.appendChild(p)
-
-    
-
-
 })
-
-expenseBtn.addEventListener('click', () => {
-    isExpense = true
-    expenseBtn.className = 'bg-white px-8 font-[500] rounded-md'
-    incomeBtn.classList.remove('bg-white')
-
-    nameInput
-})
-
-incomeBtn.addEventListener('click', () => {
-    isIncome = true
-    incomeBtn.className = ('bg-white px-8 font-[500] rounded-md')
-    expenseBtn.classList.remove('bg-white')
-})
-    
